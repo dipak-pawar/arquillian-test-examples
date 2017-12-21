8a5a535e066f4bf65e68c1fbbdb9a8f6cf60f912
Dipak Pawar <dpawar@redhat.com>
2017-12-21 16:16:02 +0530

    Adds junit5 vintage example for new arquillian rule

---
+++ junit5-arquillian-rules/src/main/java/org/arquillian/junit5/Greeter.java
@@ -0,0 +1,9 @@
+package org.arquillian.junit5;
+
+public class Greeter {
+
+   public String greet(String userName) {
+      return "Hello, " + userName;
+   }
+}
+
---
+++ junit5-arquillian-rules/src/test/java/org/arquillian/junit5/GreeterTest.java
@@ -0,0 +1,80 @@
+package org.arquillian.junit5;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.ArquillianTest;
+import org.jboss.arquillian.junit.ArquillianTestClass;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import javax.inject.Inject;
+
+//@RunWith(Arquillian.class)
+public class GreeterTest {
+
+   @ClassRule
+   public static ArquillianTestClass arquillianTestClass = new ArquillianTestClass();
+
+   @Rule
+   public ArquillianTest arquillianTest = new ArquillianTest();
+
+   @Inject
+   Greeter greeter;
+
+   @Deployment
+   public static JavaArchive createDeployment() {
+      return ShrinkWrap.create(JavaArchive.class)
+         .addClass(Greeter.class)
+         .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+   }
+
+   @Test
+   public void should_greet_earthlings() throws Exception {
+      String name = "Earthlings";
+      System.out.println("====================");
+      System.out.println("====================");
+      System.out.println(greeter.greet(name));
+      System.out.println("====================");
+      System.out.println("====================");
+      Assert.assertEquals("" + name, greeter.greet(name));
+   }
+
+   @Test
+   public void should_greet_earthlings_1() throws Exception {
+      String name = "Earthlings";
+      System.out.println("====================");
+      System.out.println("====================");
+      System.out.println(greeter.greet(name));
+      System.out.println("====================");
+      System.out.println("====================");
+      Assert.assertEquals("" + name, greeter.greet(name));
+   }
+
+   @Test
+   public void should_greet_earthlings_2() throws Exception {
+      String name = "Earthlings";
+      System.out.println("====================");
+      System.out.println("====================");
+      System.out.println(greeter.greet(name));
+      System.out.println("====================");
+      System.out.println("====================");
+      Assert.assertEquals("" + name, greeter.greet(name));
+   }
+
+   @Test@Ignore
+   public void should_greet_earthlings_3() throws Exception {
+      String name = "Earthlings";
+      System.out.println("====================");
+      System.out.println("====================");
+      System.out.println(greeter.greet(name));
+      System.out.println("====================");
+      System.out.println("====================");
+      Assert.assertEquals("" + name, greeter.greet(name));
+   }
+}
